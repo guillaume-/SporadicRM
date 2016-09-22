430a1530627caa8f81482150cae3c339dcaaf6f4
guillaume- <guillaume.ri.du@gmail.com>
2016-09-22 09:22:12 +0200

    CNS computed

--- sporadic.c
+++ sporadic.c
@@ -1,6 +1,8 @@
 /* DESCHASTRES Thomas
  * RIPOLL Guillaume
+ * gcc -lm
 */
+#include <math.h> // ceil
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -39,31 +41,31 @@ Ps = période du serveur (= Ds deadline)\n", progname);
  * calcule une itération du théorème de Lehoczky et al.
 */
 double iterate_CNS(int i, int k, int m, 
-				   a_tache *a_tasks, int nb_a_tasks,
-				   p_tache *p_tasks, int nb_p_tasks)
+				   p_tache *tasks, int nb_tasks)
 {
 	double d = 0.;
 	for(int j = 0; j<=i; ++j){
-		
+		d += tasks[j].charge * ceil( (m*tasks[k].t) / tasks[j].t);
 	}
+	d /= (m*tasks[k].t);
+	return d;
 }
 
 /* Test d'ordonnançabilité par Condition Nécessaire Suffisante
  * retourne 0 si ordonnançable, -1 sinon.
 */
-char CNS(a_tache *a_tasks, int nb_a_tasks,
-		 p_tache *p_tasks, int nb_p_tasks)
+char CNS(p_tache *p_tasks, int nb_p_tasks)
 {
 	int sigma = 0;
 	char is_i_schedulable;
-	for(int i = 0; i<nb_a_tasks+nb_p_tasks; ++i)
+	for(int i = 0; i<nb_p_tasks; ++i)
 	{
 		for(int k = 0; k<i; ++k)
 		{
 			is_i_schedulable = -1;
-			for(int m = 0; m<i; ++m)
+			for(int m = 1; m<i+1; ++m)
 			{
-				if(iterate_CNS(i, k, m, a_tasks, nb_a_tasks, p_tasks, nb_p_tasks) <= 1.)
+				if(iterate_CNS(i, k, m, p_tasks, nb_p_tasks) <= 1.)
 				{
 					is_i_schedulable = 1;
 					break;
